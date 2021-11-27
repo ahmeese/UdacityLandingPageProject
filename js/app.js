300301f62a61bbddc322dfb75cf7a7fe8ddd284d
ahmeese <73273348+ahmeese@users.noreply.github.com>
2021-11-27 06:45:55 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,19 +1,18 @@
 //Defining global variables
-var sections =document.querySelectorAll("section");
-var nav_menu =document.getElementById('navbar__list');
-
+let sections =document.querySelectorAll("section");
+let nav_menu =document.getElementById('navbar__list');
 
 //Creating the nav items
 function navList(){
     for(elem of sections){
-        var section_id=elem.id;
-        var section_name=elem.dataset.nav;
-        var list=document.createElement('li')
+        let section_id=elem.id;
+        let section_name=elem.dataset.nav;
+        let list=document.createElement('li');
         //Adding the menu__link style to the items
-        list.innerHTML=`<a  class="menu__link" data-value="${section_id}" href="#">${section_name}</a>`
+        list.innerHTML=`<a  class="menu__link" data-value="${section_id}" href="#">${section_name}</a>`;
         nav_menu.appendChild(list);
     }
-}
+};
 
 /*
 Using IntersectionObserverApi to check if the section is in the viewport
@@ -25,39 +24,38 @@ const options ={
     rootMargin:'100px',
     //Assigning threshold value to 100% (1) to make sure that the section is fully view in the viewport
     threshold:'1'
-}
+};
 const observer = new IntersectionObserver( function (entries,observer){
     
     entries.forEach(entry =>{
        if(entry.isIntersecting){
           entry.target.classList.add("your-active-class");
        } else{
-        entry.target.classList.remove("your-active-class")
-
+        entry.target.classList.remove("your-active-class");
        }
     })
-},
-
-options)
+},options);
 
 for (section of sections){
     observer.observe(section)
-
-}
+};
 
 // build the nav
-navList()
+navList();
 
-// When clicking an item from the navigation menu, the link  scroll to the appropriate section using scrollIntoView() function.
+/*
+When clicking an item from the navigation menu, the link scroll to the appropriate section using
+scrollIntoView() function.
+*/
 
-const lists=document.querySelectorAll(".menu__link")
+const lists=document.querySelectorAll(".menu__link");
 lists.forEach((item) =>{
      item.addEventListener('click',(event)=>{
-       event.preventDefault()
-       const elem =document.getElementById(item.getAttribute("data-value"))
-       console.log(elem)
-       elem.scrollIntoView({behavior:"smooth"})
-     })
- })
+       event.preventDefault();
+       const elem =document.getElementById(item.getAttribute("data-value"));
+       console.log(elem);
+       elem.scrollIntoView({behavior:"smooth"});
+     });
+ });
 
 
